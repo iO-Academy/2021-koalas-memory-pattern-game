2c4f0eff9c29e265ced685de1ef24d655cce5327
Estera <estera.maftei94@gmail.com>
2021-06-15 13:12:30 +0100

    end modal

---
+++ script.js
@@ -0,0 +1,34 @@
+let trigger = document.querySelector('.trigger');
+let modal = document.querySelector('.modal')
+let closeBtn = document.querySelector('.close-button')
+
+trigger.addEventListener("click", e => {
+    modal.classList.toggle("show-modal");
+});
+
+closeBtn.addEventListener("click", e => {
+    modal.classList.toggle("show-modal")
+});
+
+
+
+
+
+
+let modalEndGame = document.querySelector('#endModal')
+let tile = document.querySelectorAll('.tile')
+
+tile.forEach(element =>
+    element.addEventListener('click', handleClick)
+)
+
+function handleClick (e) {
+    e.stopPropagation();
+    modalEndGame.classList.toggle("blockEndGame")
+}
+
+window.onclick = function(e) {
+    if (e.target === modalEndGame) {
+        modalEndGame.classList.toggle("blockEndGame");
+    }
+}
