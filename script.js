5c47e90374bb47657e23f6219ffea00d2d625d7b
Alicja <puranut@hotmail.com>
2021-06-15 10:53:17 +0100

    adding classes onclick

--- script.js
+++ script.js
@@ -8,14 +8,39 @@ function select() {
     return random
 }
 
-select()
+let random = select()
 
 
 document.querySelector('.start-button').addEventListener('click',e => {
-    console.log('clicked')
+    random.forEach(colour => {
+        document.querySelector('#' + colour).classList.add('yellow')
+    })
 })
 
 
-random.forEach(colour => {
-    document.querySelector('#colour').
-})
+
+
+// let colours = [];
+// colours.push(document.querySelector('#red'))
+// colours.push(document.querySelector('#green'))
+// colours.push(document.querySelector('#blue'))
+// colours.push(document.querySelector('#yellow'))
+//
+// function select(colours) {
+//     let random = []
+//     for (let i = 0; i < 4; i++) {
+//         random.push(colours[Math.floor(Math.random() * colours.length)]);
+//     }
+//     console.log(random)
+//     return random
+// }
+// select(colours)
+// // document.querySelector('.start-button').addEventListener('click',e => {
+// //     console.log('clicked')
+// // })
+// // setTimeout(select(), 2000)
+// //
+// random = select()
+// random.forEach(colour => {
+//     colour.classList.add()
+// })
