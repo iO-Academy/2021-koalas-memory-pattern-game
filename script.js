7f0bae124b10415bc98435e67d65a9179c185fbb
SniffleSneeze <70772425+SniffleSneeze@users.noreply.github.com>
2021-06-15 16:18:01 +0100

    code update

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-let computerArray = ['green', 'yellow', 'red', 'blue'];
-let lvlCount = 1;
-
-function playerTurn(computerArray) {
-    document.querySelectorAll('.box').forEach(element => {
-        element.addEventListener('click', e => {
-            if (e.target.id !== computerArray[0]) {
-                console.log('error') // function error message
-            } else {
-                computerArray.shift();
-                if (computerArray.length === 0) {
-                    lvlCount++;
-                    console.log('you have reach lvl ' + lvlCount);
-                }
-            }
-        })
-    })
-}
-
-playerTurn(computerArray)
-
 const modal = document.querySelector(".modal");
 
 document.querySelector(".trigger").addEventListener("click", e => {
@@ -31,16 +10,29 @@ document.querySelector(".close-button").addEventListener("click", e => {
 
 function modalEndGame () {
     let modalEndGame = document.querySelector('#endModal')
-    let tile = document.querySelectorAll('.box')
-
-    tile.forEach(element =>
-        element.addEventListener('click', handleClick)
-    )
-
-    function handleClick(e) {
-        e.stopPropagation();
         modalEndGame.classList.toggle("blockEndGame")
-    }
 }
 
-modalEndGame();
+
+let computerArray = ['green', 'yellow', 'red', 'blue'];
+let lvlCount = 1;
+
+function playerTurn(computerArray) {
+    document.querySelectorAll('.box').forEach(element => {
+        element.addEventListener('click', e => {
+            e.stopPropagation();
+            if (e.target.id !== computerArray[0]) {
+                modalEndGame()
+            } else {
+                console.log('correct')
+                computerArray.shift();
+                if (computerArray.length === 0) {
+                    lvlCount++;
+                    console.log('you have reach lvl ' + lvlCount);
+                }
+            }
+        })
+    })
+}
+
+playerTurn(computerArray)
