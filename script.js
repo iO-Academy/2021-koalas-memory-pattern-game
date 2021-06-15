437de6ea19dc35bbe486e24294907a1c1bf8b44d
Saskia <saskia_west1996@hotmail.co.uk>
2021-06-15 15:51:29 +0100

    add comment

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ document.querySelector(".trigger").addEventListener("click", e => {
 document.querySelector(".close-button").addEventListener("click", e => {
     modal.classList.toggle("show-modal");
 });
+
+// Make game start when button is clicked
+// const startGame = document.querySelector('.start-button');
+//
+// startGame.addEventListener('click', e => {
+//     // insert code for randomize pattern sequence
+//     console.log('hello')
+// });
