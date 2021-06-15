d85f2f390a8a6cae623a9d3b690e7a062bc109ba
alina <alinaneculai19@gmail.com>
2021-06-15 14:03:33 +0100

    added colours to boxes

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const modal = document.querySelector(".modal");
-const trigger = document.querySelector(".trigger");
-const closeButton = document.querySelector(".close-button");
 
-trigger.addEventListener("click", e => {
+document.querySelector(".trigger").addEventListener("click", e => {
     modal.classList.toggle("show-modal");
 });
 
-closeButton.addEventListener("click", e => {
+document.querySelector(".close-button").addEventListener("click", e => {
     modal.classList.toggle("show-modal");
 });
