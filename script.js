d53c634e52f95aff816ea1f99e84ae140e378bea
Alicja <puranut@hotmail.com>
2021-06-15 14:49:45 +0100

    randomising function done and working

--- script.js
+++ script.js
@@ -1,46 +1,17 @@
-function select() {
-    let random = []
-    let colours = ['red', 'green', 'blue', 'yellow'];
-    for (let i = 0; i < 4; i++) {
-        random.push(colours[Math.floor(Math.random() * colours.length)]);
+let timesRun = 1
+function lightUpSquare() {
+    let colours = ["blue", "green", "yellow", "red"]
+    let random_colour = (colours[Math.floor(Math.random()*colours.length)])
+    document.querySelector('#' + random_colour).classList.add('activated')
+    setTimeout(() => {
+        removeLight(random_colour)
+    }, 500)
+    if (timesRun < 4) {
+        timesRun++
+        setTimeout(lightUpSquare, 2000)
     }
-    console.log(random)
-    return random
 }
-
-let random = select()
-
-
-document.querySelector('.start-button').addEventListener('click',e => {
-    random.forEach(colour => {
-        document.querySelector('#' + colour).classList.add('yellow')
-    })
-})
-
-
-
-
-// let colours = [];
-// colours.push(document.querySelector('#red'))
-// colours.push(document.querySelector('#green'))
-// colours.push(document.querySelector('#blue'))
-// colours.push(document.querySelector('#yellow'))
-//
-// function select(colours) {
-//     let random = []
-//     for (let i = 0; i < 4; i++) {
-//         random.push(colours[Math.floor(Math.random() * colours.length)]);
-//     }
-//     console.log(random)
-//     return random
-// }
-// select(colours)
-// // document.querySelector('.start-button').addEventListener('click',e => {
-// //     console.log('clicked')
-// // })
-// // setTimeout(select(), 2000)
-// //
-// random = select()
-// random.forEach(colour => {
-//     colour.classList.add()
-// })
+function removeLight(random_colour) {
+    document.querySelector('#' + random_colour).classList.remove('activated')
+}
+// lightUpSquare() <-- add to start button event listener
