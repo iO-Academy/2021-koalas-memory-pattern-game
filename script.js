5fec1378d4e7bd041f710acf68d8e85084d5ee0b
Estera <estera.maftei94@gmail.com>
2021-06-16 13:05:19 +0100

    cleaned up the code

--- script.js
+++ script.js
@@ -8,17 +8,16 @@ document.querySelector(".close-button").addEventListener("click", e => {
     modal.classList.toggle("show-modal");
 });
 
-function modalEndGame () {
-    let modalEndGame = document.querySelector('#endModal')
-        modalEndGame.classList.toggle("blockEndGame")
+function toggleEndGameModal () {
+        document.querySelector('#endModal').classList.toggle("showEndGameModal")
 }
 
 const display = document.querySelector('.display')
 let colorPattern = []
 let timesRun = 1
-function lightUpSquare() {
-    let colours = ["blue", "green", "yellow", "red"]
-    let random_colour = (colours[Math.floor(Math.random()*colours.length)])
+function lightUpRandomSquare() {
+    const colours = ["blue", "green", "yellow", "red"]
+    const random_colour = (colours[Math.floor(Math.random()*colours.length)])
     colorPattern.push(random_colour)
     document.querySelector('#' + random_colour).classList.add('activated')
     display.classList.add('unclickable');
@@ -27,7 +26,7 @@ function lightUpSquare() {
     }, 500)
     if (timesRun < 4) {
         timesRun++
-        setTimeout(lightUpSquare, 1000)
+        setTimeout(lightUpRandomSquare, 1000)
     } else {
         display.classList.remove('unclickable');
         playerTurn()
@@ -38,10 +37,8 @@ function removeLight(random_colour) {
     document.querySelector('#' + random_colour).classList.remove('activated')
 }
 
-const startGame = document.querySelector('.start-button');
-
-startGame.addEventListener('click', e => {
-    lightUpSquare()
+document.querySelector('.start-button').addEventListener('click', e => {
+    lightUpRandomSquare()
     display.classList.remove('unclickable');
 })
 
@@ -51,17 +48,21 @@ function playerTurn() {
     })
 }
 
+function removeEventListener () {
+    if (colorPattern.length === 0) {
+        timesRun = 1
+        document.querySelectorAll('.box').forEach(element => {
+            element.removeEventListener('click',  boxClick)
+        })
+        setTimeout(lightUpRandomSquare, 1000)
+    }
+}
+
 function boxClick(e) {
     if (e.target.id !== colorPattern[0]) {
-        modalEndGame()
+        toggleEndGameModal()
     } else {
         colorPattern.shift()
-        if (colorPattern.length === 0) {
-            timesRun = 1
-            document.querySelectorAll('.box').forEach(element => {
-                element.removeEventListener('click',  boxClick)
-            })
-            setTimeout(lightUpSquare, 2000)
-        }
+        removeEventListener()
     }
 }
